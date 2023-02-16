faf049549b5cbc3852b5acf6fb699a318163f8a9
Gesserok <if2102@outlook.com>
2023-02-16 10:38:02 +0200

    weather

--- src/main/kotlin/org/fop/weather/dto/TestControllerResponse.kt
+++ src/main/kotlin/org/fop/weather/dto/TestControllerResponse.kt
@@ -2,5 +2,6 @@ package org.fop.weather.dto
 
 data class TestControllerResponse(
     val msg: String = "Hello from Weather Service",
-    val ip: String
+    val ip: String,
+    val weather: String = "The weather is fine"
 )
